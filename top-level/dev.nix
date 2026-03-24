4bf8f87a5b103a6bb00e2490cf62eff95821dc49
fmway <fm18lv@gmail.com>
2026-03-24 20:54:12 +0700

    Update lockfile

--- top-level/dev.nix
+++ top-level/dev.nix
@@ -4,10 +4,10 @@
   {
     devShells.default = pkgs.mkShell {
       packages = [
-        (inputs.clan-core.packages.${system}
+        (inputs.clan-core.packages.${system}.clan-cli
         # optional: temporary patches, just to apply disko, really unnecessary
          /*
-        .clan-cli.overrideAttrs (o: {
+        .overrideAttrs (o: {
           patches = o.patches or [] ++ [
             (pkgs.replaceVars ./00-clan-template-disk.patch {
               template_dir = "${self.outPath}/templates";
